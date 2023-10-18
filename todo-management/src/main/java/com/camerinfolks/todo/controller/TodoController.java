bfd724518eee79c103cbfa701edb8028d5bcc2bc
MrUnwonted <arjungodzarjun@gmail.com>
2023-10-18 11:11:40 +0530

    Spring Security Added

--- todo-management/src/main/java/com/camerinfolks/todo/controller/TodoController.java
+++ todo-management/src/main/java/com/camerinfolks/todo/controller/TodoController.java
@@ -1,7 +1,6 @@
 package com.camerinfolks.todo.controller;
 
 import com.camerinfolks.todo.dto.TodoDto;
-import com.camerinfolks.todo.entity.Todo;
 import com.camerinfolks.todo.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -51,6 +50,13 @@ public class TodoController {
     @PatchMapping("{id}/complete")
     public ResponseEntity<TodoDto> completeTodo(@PathVariable("id") Long todoId){
         TodoDto updatedTodo = todoService.completeTodo(todoId);
+
+        return ResponseEntity.ok(updatedTodo);
+    }
+
+    @PatchMapping("{id}/in-complete")
+    public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable("id") Long todoId){
+        TodoDto updatedTodo = todoService.inCompleteTodo(todoId);
 
         return ResponseEntity.ok(updatedTodo);
     }
