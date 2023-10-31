f7d354631a55b4c903e50b3198d0947e5db7f0e1
MrUnwonted <arjungodzarjun@gmail.com>
2023-10-31 12:47:08 +0530

    Spring Security Database

---
+++ todo-management/src/main/java/com/camerinfolks/todo/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.camerinfolks.todo.repository;
+
+import com.camerinfolks.todo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
