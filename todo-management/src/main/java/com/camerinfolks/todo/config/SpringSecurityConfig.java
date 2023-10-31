100d1a64b06d7fafa88395406fc93318e4e1b7da
MrUnwonted <arjungodzarjun@gmail.com>
2023-10-31 14:39:35 +0530

    DAO Authentication

--- todo-management/src/main/java/com/camerinfolks/todo/config/SpringSecurityConfig.java
+++ todo-management/src/main/java/com/camerinfolks/todo/config/SpringSecurityConfig.java
@@ -1,26 +1,28 @@
 package com.camerinfolks.todo.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-
 
 
 
 @Configuration
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SpringSecurityConfig {
+
+    public UserDetailsService userDetailsService;
+
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -45,24 +47,29 @@ public class SpringSecurityConfig {
     }
 
 
-
     @Bean
-    public UserDetailsService userDetailsService(){
-
-        UserDetails arjun= User.builder()
-                .username("arjun")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(arjun,admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+            return configuration.getAuthenticationManager();
     }
+
+//  In memory authentication in spring security
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//
+//        UserDetails arjun= User.builder()
+//                .username("arjun")
+//                .password(passwordEncoder().encode("123"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(arjun,admin);
+//    }
 
 
 }
